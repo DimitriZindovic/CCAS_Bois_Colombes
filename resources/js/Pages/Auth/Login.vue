9b32f02be818d6e29f366c8cd1e84d305b491ed8
Leo-Riche <leo.riche@edu.devinci.fr>
2024-04-03 11:30:12 +0200

    :sparkles: Adding css for Login page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,6 +1,5 @@
 <script setup>
 import Checkbox from "@/Components/Checkbox.vue";
-import GuestLayout from "@/Layouts/GuestLayout.vue";
 import InputError from "@/Components/InputError.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
@@ -30,13 +29,13 @@ const submit = () => {
 </script>
 
 <template>
-    <GuestLayout>
-        <Head title="Log in" />
+    <Head title="Log in" />
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
+    <div v-if="status">
+        {{ status }}
+    </div>
 
+    <div class="form">
         <form @submit.prevent="submit">
             <div>
                 <InputLabel for="email" value="Email" />
@@ -44,55 +43,126 @@ const submit = () => {
                 <TextInput
                     id="email"
                     type="email"
-                    class="mt-1 block w-full"
                     v-model="form.email"
                     required
                     autofocus
                     autocomplete="username"
                 />
 
-                <InputError class="mt-2" :message="form.errors.email" />
+                <InputError :message="form.errors.email" />
             </div>
 
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+            <div>
+                <InputLabel for="password" value="Mot de passe" />
 
                 <TextInput
                     id="password"
                     type="password"
-                    class="mt-1 block w-full"
                     v-model="form.password"
                     required
                     autocomplete="current-password"
                 />
 
-                <InputError class="mt-2" :message="form.errors.password" />
+                <InputError :message="form.errors.password" />
             </div>
 
-            <div class="block mt-4">
-                <label class="flex items-center">
+            <div>
+                <label>
                     <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600">Remember me</span>
+                    <span>Se souvenir de moi</span>
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Forgot your password?
+            <div class="login">
+                <Link v-if="canResetPassword" :href="route('password.request')">
+                    Mot de passe oublié ?
                 </Link>
 
                 <PrimaryButton
-                    class="ms-4"
                     :class="{ 'opacity-25': form.processing }"
                     :disabled="form.processing"
                 >
-                    Log in
+                    Se connecter
                 </PrimaryButton>
             </div>
         </form>
-    </GuestLayout>
+    </div>
 </template>
+
+<style scoped lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@100&display=swap");
+
+.form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    height: 100vh;
+
+    font-size: 34px;
+    font-family: "Open Sans", sans-serif;
+    color: #000000;
+    padding-bottom: 16px;
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+        padding: 32px;
+        box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
+        border-radius: 12px;
+        font-size: 1.25rem;
+        width: 25%;
+
+        div {
+            display: flex;
+            flex-direction: column;
+            gap: 0.5rem;
+            width: 100%;
+
+            label {
+                display: flex;
+                gap: 0.5rem;
+                align-items: center;
+                font-size: 1.25rem;
+            }
+
+            input {
+                padding: 0.5rem;
+                font-size: 1.25rem;
+                border: 1px solid #000000;
+                border-radius: 4px;
+            }
+        }
+
+        .login {
+            gap: 2rem;
+            text-align: center;
+
+            button {
+                background-color: #1f266b;
+                color: #ffffff;
+                border: none;
+                border-radius: 4px;
+                width: 100%;
+                height: 50px;
+                font-size: 1.25rem;
+            }
+
+            a {
+                color: #1f266b;
+                font-size: 1.25rem;
+                text-decoration: none;
+                margin-top: 1rem;
+            }
+        }
+
+        InputLabel {
+            font-size: 1.25rem;
+        }
+    }
+}
+</style>
